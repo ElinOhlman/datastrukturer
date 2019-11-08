cb50c7738bf2f0e9693b8520b9117615c12d49d6
fyratresju <elin.ohlman@gmail.com>
2019-11-08 16:26:45 +0100

    inget ändrat

--- Upp1_2_6.java
+++ Upp1_2_6.java
@@ -6,7 +6,6 @@ class Upp1_2_6{
     else{
       System.out.println("Not circular rotation");
     }
-
   }
   public static boolean circular(String word1, String word2){
     return (word1.length() == word2.length()) && (word1.concat(word1).indexOf(word2) >= 0);
