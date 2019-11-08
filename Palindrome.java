c4ae64e51a03f948eabe5b2316e92aed266865e0
fyratresju <elin.ohlman@gmail.com>
2019-11-08 16:58:58 +0100

    extended version of palindrome

--- Palindrome.java
+++ Palindrome.java
@@ -1,18 +1,27 @@
 class Palindrome{
   public static void main(String[] args) {
-    if (isPalindrome("madamimadam")){
+    if (isPalindrome("Madam, I'm Adam!")){
       System.out.println("palindrome");
     }
     else{
       System.out.println("inte palindrome");
     }
-
   }
   public static boolean isPalindrome(String string){
-    for (int i = 0; i< string.length(); i++){
-      if (string.charAt(i) != string.charAt(string.length()-1-i)){
+    int posL = 0;
+    int posR = string.length()-2;
+    while (posL < posR){
+      while (!Character.isAlphabetic(string.charAt(posL))){
+        posL++;
+      }
+      while (!Character.isAlphabetic(string.charAt(posR))){
+        posR--;
+      }
+      if (Character.toUpperCase(string.charAt(posR)) != Character.toUpperCase(string.charAt(posL))){
         return false;
       }
+      posL++;
+      posR--;
     }
     return true;
   }
